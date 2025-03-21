59ae7e3aafec48c47ff9d41db0e577584c76bb23
youzarou17 <121348591+youzarou17@users.noreply.github.com>
2025-03-21 17:38:12 +0000

    add requirements

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using `make_response`.
-    """
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])  # Ajout de la route et des méthodes HTTP
+def hello_world():
+    """Responds to any HTTP request."""
     request_json = request.get_json(silent=True)
     if request_json and 'name' in request_json:
         name = request_json['name']
     else:
         name = 'World'
     return f'Hello {name}!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
